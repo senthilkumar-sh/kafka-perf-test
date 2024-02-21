cbebfe074eaa667a2821625f3d39eb0081f947ce
Senthilkumar Shanmugam <sensub2k@yahoo.com>
2024-02-21 17:08:46 -0600

    Kafka code

--- index.js
+++ index.js
@@ -1,132 +1,154 @@
-import { check } from "k6"
-import {
-  Writer,
-  Reader,
-  Connection,
-  SchemaRegistry,
-  SCHEMA_TYPE_JSON,
-  SASL_AWS_IAM,
-  TLS_1_2,
-} from "k6/x/kafka"
+//const producer = require('./kafka/producer-process')
+const express = require('express')
+const routes = require('./routes')
+const app = express()
+const cors = require('cors')
+const _ = require('lodash');
+const port = 3000
 
-export const options = {
-  scenarios: {
-    sasl_auth: {
-      executor: "constant-vus",
-      vus: 1,
-      duration: "10s",
-      gracefulStop: "1s",
-    },
-  },
-}
+// Third-Party Middleware
 
-const brokers = [
-  "b-1.myTestCluster.123z8u.c2.kafka.us-west-1.amazonaws.com:9098",
-  "b-2.myTestCluster.123z8u.c2.kafka.us-west-1.amazonaws.com:9098",
-  "b-3.myTestCluster.123z8u.c2.kafka.us-west-1.amazonaws.com:9098",
-]
-const topic = "xk6_kafka_json_topic"
-const saslConfig = {
-  algorithm: SASL_AWS_IAM,
-}
+app.use(cors());
 
-const tlsConfig = {
-  enableTls: true,
-  insecureSkipTlsVerify: true,
-  minVersion: TLS_1_2,
-}
+// Built-In Middleware
 
-const offset = 0
-const partition = 0
-const numPartitions = 1
-const replicationFactor = 1
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/perf', routes.process);
 
-const writer = new Writer({
-  brokers: brokers,
-  topic: topic,
-  sasl: saslConfig,
-  tls: tlsConfig,
+//let producedData = '2 records sent, 8.032129 records/sec (0.01 MB/sec), 128.50 ms avg latency, 241.00 ms max latency, 241 ms 50th, 241 ms 95th, 241 ms 99th, 241 ms 99.9th.\n';
+// let producedData = '20528 records sent, 4105.6 records/sec (3.92 MB/sec), 1760.7 ms avg latency, 3313.0 ms max latency., 25536 records sent, 5107.2 records/sec (4.87 MB/sec), 5166.5 ms avg latency, 6839.0 ms max latency., 23985 records sent, 4797.0 records/sec (4.57 MB/sec), 8090.6 ms avg latency, 9098.0 ms max latency., 17230 records sent, 3446.0 records/sec (3.29 MB/sec), 10677.6 ms avg latency, 11854.0 ms max latency., 30157 records sent, 6031.4 records/sec (5.75 MB/sec), 12561.9 ms avg latency, 13075.0 ms max latency., 59911 records sent, 11982.2 records/sec (11.43 MB/sec), 10605.9 ms avg latency, 12633.0 ms max latency., 200000 records sent, 6293.860339 records/sec (6.00 MB/sec), 8712.20 ms avg latency, 13075.00 ms max latency, 9111 ms 50th, 12825 ms 95th, 13014 ms 99th, 13073 ms 99.9th.,'
+// let producedDataArr = _.split(producedData, ', ')
+
+// console.log(producedDataArr)
+// let produced = {}
+// let pDataArr = []
+
+// _.forEach(producedDataArr, function (data, i) {
+//     let pData = {}
+//     arr = _.split(data, ' ');
+//     console.log(arr);
+//     pData.value = arr[0]
+//     pData.unit = arr[1]
+//     pData.response = arr[2]
+//     if (arr[3]) {
+//         pData.label = arr[3]
+//     }
+    
+//     if (!produced.records && arr[1] == 'records') {
+//         produced.totalHits = 1;
+//         let records = {}
+//         records.value = _.toNumber(arr[0])
+//         records.response = arr[2]
+//         produced.records = records
+//     } else if (arr[1] == 'records') {
+//         produced.totalHits += 1;
+//         let records = produced.records
+//         records.value = records.value + _.toNumber(arr[0])
+//         produced.records = records
+//     }
+
+//     if (!produced.recordsSec && arr[1] == 'records/sec') {
+//         let recordsSec = {}
+//         recordsSec.value = _.toNumber(arr[0])
+//         recordsSec.unit = arr[1]
+//         recordsSec.response = arr[2] + _.replace(arr[3], '.', '')
+//         produced.recordsSec = recordsSec
+//     } else if (arr[1] == 'records/sec') {
+//         let recordsSec = produced.recordsSec
+//         recordsSec.value = recordsSec.value + _.toNumber(arr[0])
+//         produced.recordsSec = recordsSec
+//     }
+
+//     if (!produced.avg && arr[2] == 'avg') {
+//         let avg = {}
+//         avg.value = _.toNumber(arr[0])
+//         avg.unit = arr[1]
+//         avg.response = arr[3]
+//         produced.avg = avg
+//     } else if (arr[2] == 'avg') {
+//         let avg = produced.avg
+//         avg.value = (avg.value + _.toNumber(arr[0]))
+//         produced.avg = avg
+//     }
+
+//     if (!produced.max && arr[2] == 'max') {
+//         let max = {}
+//         max.value = _.toNumber(arr[0])
+//         max.unit = arr[1]
+//         max.response =  _.replace(arr[3], '.', '')
+//         produced.max = max
+//     } else if (arr[2] == 'max') {
+//         let max = produced.max
+//         max.value = (max.value > _.toNumber(arr[0])) ? max.value : _.toNumber(arr[0])
+//         produced.max = max
+//     }
+
+//     if (!produced.percentile50th && arr[2] == '50th') {
+//         console.log('index = ', i)
+//         let avg = produced.avg
+//         produced.avg.value = produced.avg.value / produced.totalHits
+//         let fiftieth = {}
+//         fiftieth.value = _.toNumber(arr[0])
+//         fiftieth.unit = arr[1]
+//         produced.percentile50th = fiftieth
+//     }
+
+//     if (!produced.percentile95th && arr[2] == '95th') {
+//         let ninetyFifth = {}
+//         ninetyFifth.value = _.toNumber(arr[0])
+//         ninetyFifth.unit = arr[1]
+//         produced.percentile95th = ninetyFifth
+//     }
+
+//     if (!produced.percentile99th && arr[2] == '99th') {
+//         let ninetyNinth = {}
+//         ninetyNinth.value = _.toNumber(arr[0])
+//         ninetyNinth.unit = arr[1]
+//         produced.percentile99th = ninetyNinth
+//     }
+
+//     if (!produced.percentile99Point9th && arr[2] == '99.9th') {
+//         let ninetyNinth9th = {}
+//         ninetyNinth9th.value = _.toNumber(arr[0])
+//         ninetyNinth9th.unit = arr[1]
+//         produced.percentile99Point9th = ninetyNinth9th
+//     }
+
+//     pDataArr.push(pData)
+// });
+//console.log(produced)
+
+// let consumedData = 'WARNING: option [threads] and [num-fetch-threads] have been deprecated and will be ignored by the test\nstart.time, end.time, data.consumed.in.MB, MB.sec, data.consumed.in.nMsg, nMsg.sec, rebalance.time.ms, fetch.time.ms, fetch.MB.sec, fetch.nMsg.sec\nWARNING: Exiting before consuming the expected number of messages: timeout (10000 ms) exceeded. You can use the --timeout option to increase the timeout.\n2024-02-20 21:20:09:315, 2024-02-20 21:20:19:720, 4.4379, 0.4265, 39304, 3777.4147, 244, 10161, 0.4368, 3868.1232\n'
+// let consumedDataArr = _.split(consumedData, '.\n')
+// console.log(consumedDataArr)
+// let cDataArr = []
+// _.forEach(consumedDataArr, function (data, i) {
+    
+//     if (i == 1) {
+//         console.log('data = ', data)    
+//          let cData = {}
+//          arr = _.split(data, ',');
+//     //     console.log(arr);
+//            cData.startTime = arr[0]
+//            cData.endTime = arr[1]
+//            cData.dataConsumedInMB = arr[2]
+//            cData.mbPerSec = arr[3]
+//            cData.consumedMsg = arr[4]
+//            cData.consumedMsgSec = arr[5]
+//            cData.rebalanceTimeMs = arr[6]
+//            cData.fetchTimeMs = arr[7]
+//            cData.fetchMBSec = arr[8]
+//            cData.fetchMessageSec = _.replace(arr[9], '\n', '');
+//     //     pData.unit = arr[1]
+//     //     pData.response = arr[2]
+//     //     if (arr[3]) {
+//     //         pData.label = arr[3]
+//     //     }
+//          cDataArr.push(cData)
+//     }
+// });
+// console.log(cDataArr)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-const reader = new Reader({
-  brokers: brokers,
-  topic: topic,
-  partition: partition,
-  offset: offset,
-  sasl: saslConfig,
-  tls: tlsConfig,
-})
-const connection = new Connection({
-  address: brokers[0],
-  sasl: saslConfig,
-  tls: tlsConfig,
-})
-const schemaRegistry = new SchemaRegistry()
-
-if (__VU == 0) {
-  connection.createTopic({
-    topic: topic,
-    numPartitions: numPartitions,
-    replicationFactor: replicationFactor,
-  })
-  console.log("Existing topics: ", connection.listTopics(saslConfig, tlsConfig))
-}
-
-export default function () {
-  for (let index = 0; index < 100; index++) {
-    let messages = [
-      {
-        key: schemaRegistry.serialize({
-          data: {
-            correlationId: "test-id-abc-" + index,
-          },
-          schemaType: SCHEMA_TYPE_JSON,
-        }),
-        value: schemaRegistry.serialize({
-          data: {
-            name: "xk6-kafka",
-          },
-          schemaType: SCHEMA_TYPE_JSON,
-        }),
-      },
-      {
-        key: schemaRegistry.serialize({
-          data: {
-            correlationId: "test-id-def-" + index,
-          },
-          schemaType: SCHEMA_TYPE_JSON,
-        }),
-        value: schemaRegistry.serialize({
-          data: {
-            name: "xk6-kafka",
-          },
-          schemaType: SCHEMA_TYPE_JSON,
-        }),
-      },
-    ]
-
-    writer.produce({ messages: messages })
-  }
-
-  let messages = reader.consume({ limit: 10 })
-  check(messages, {
-    "10 messages returned": (msgs) => msgs.length == 10,
-    "key is correct": (msgs) =>
-      schemaRegistry
-        .deserialize({ data: msgs[0].key, schemaType: SCHEMA_TYPE_JSON })
-        .correlationId.startsWith("test-id-"),
-    "value is correct": (msgs) =>
-      schemaRegistry.deserialize({
-        data: msgs[0].value,
-        schemaType: SCHEMA_TYPE_JSON,
-      }).name == "xk6-kafka",
-  })
-}
-
-export function teardown(data) {
-  if (__VU == 0) {
-    connection.deleteTopic(topic)
-  }
-  writer.close()
-  reader.close()
-  connection.close()
-}
